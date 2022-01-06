7d3cf88f3ecfdcc6f76bdca52a6ac7b9e7dbbbf9
Marcelly Marsura <marcellymarsura@gmail.com>
2022-01-06 09:38:59 -0300

    1000 adicionado

---
+++ Desafios/1000.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+ //BEECROWD (URI)
+
+ //1000 | Hello World! | Iniciante
+
+#region Enunciado
+    /* 
+     * Bem-vindo ao beecrowd!
+     * O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
+     * Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+     */
+#endregion
+
+//por Marcelly Marsura
+
+namespace beecrowd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    
+}
